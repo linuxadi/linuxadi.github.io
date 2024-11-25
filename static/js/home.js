3deeab59f2875cb3620415204195ec29bab507fb
Aditya singh <84119876+linuxadi@users.noreply.github.com>
2024-11-25 05:26:39 -0500

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,17 @@
-console.log("Welcome to your personal GitHub page!");
+$(document).ready(function () {
+  let commands = [
+    "ls -1 links/",
+    "cat about.txt",
+    "ls /",
+  ];
+  
+  let delay = 0;
+
+  $(".animated-command").each(function (index) {
+    let element = $(this);
+    setTimeout(() => {
+      element.css("animation", "typing 2s steps(20, end), blink-caret 0.75s step-end infinite");
+    }, delay);
+    delay += 2000;
+  });
+});
